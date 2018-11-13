fc0118d200d8e693497602b670abfeb12fe5b508
vchernogorov <vchernogorov@griddynamics.com>
2018-11-13 05:55:36 +0400

    Delete unused functions

--- src/main/kotlin/com/vchernogorov/discordbot/Utility.kt
+++ src/main/kotlin/com/vchernogorov/discordbot/Utility.kt
@@ -5,26 +5,12 @@ import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.delay
 import net.dv8tion.jda.core.entities.MessageChannel
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
-import java.io.File
-import java.lang.IllegalStateException
-import java.util.stream.Collectors
 
 inline fun <reified T> Gson.fromJson(json: String) =
     this.fromJson<T>(json, object : TypeToken<T>() {}.type)
 
-fun MessageChannel.getMessages(limit: Long) =
-    this.iterableHistory.stream().limit(limit).collect(Collectors.toList())
-
 fun MessageReceivedEvent.send(message: String) =
     textChannel.sendMessage(net.dv8tion.jda.core.MessageBuilder().append(message).build())
-
-fun File.createEverything(): File {
-  if (!exists()) {
-    parentFile.mkdirs()
-    createNewFile()
-  }
-  return this
-}
 
 fun MessageChannel.getLatestMessageIdSafe(): String? {
   return try {

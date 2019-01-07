f2236fa47995cc35d64feb3cba3af0227363b7a3
vchernogorov <vchernogorov@griddynamics.com>
2019-01-07 11:18:49 +0400

    Fix formatting for MEMBER_STAT comparison message

--- src/main/kotlin/com/vchernogorov/discordbot/DiscordMessageBuilders.kt
+++ src/main/kotlin/com/vchernogorov/discordbot/DiscordMessageBuilders.kt
@@ -12,62 +12,32 @@ fun memberStatMessage(memberStat: MemberStat) = MessageBuilder()
         .append("**Active days:** ${memberStat.activeDays.size} days\n")
 
 fun memberStatCompareMessage(memberStat: MemberStat, memberStats: List<MemberStat>): MessageBuilder {
-    val commonException = Exception("Error occurred during member stats comparison. Check logs for details.")
+    val allStats = memberStats + memberStat
+    val messageBuilder = MessageBuilder()
+            .append("`[Members' statistics comparison for ${memberStat.user} and ${memberStats.map { it.user }.joinToString(separator = ",")}]`\n")
 
-    val totalMessages = with(memberStats.maxBy { it.messages } ?: throw commonException) {
-        if (memberStat.messages > this.messages) {
-            memberStat.messages - this.messages to memberStat
+    genericStatCompareMessage("Total messages", messageBuilder, allStats, memberStat, "messages") { it.messages }
+    genericStatCompareMessage("Average message length", messageBuilder, allStats, memberStat, "symbols") { it.messageLengthAvg.toInt() }
+    genericStatCompareMessage("Messages per day", messageBuilder, allStats, memberStat, "messages") { it.messagesPerDay.toInt() }
+    genericStatCompareMessage("Messages per active day", messageBuilder, allStats, memberStat, "messages") { it.messagesPerActiveDay.toInt() }
+    genericStatCompareMessage("Days in guild", messageBuilder, allStats, memberStat, "days") { it.daysInGuild }
+    genericStatCompareMessage("Active days", messageBuilder, allStats, memberStat, "days") { it.activeDays.size }
+
+    return messageBuilder
+}
+
+private fun genericStatCompareMessage(title: String, messageBuilder: MessageBuilder, allStats: List<MemberStat>, memberStat: MemberStat, postfix: String, getField: (MemberStat) -> Int) {
+    messageBuilder.append("**$title:**\n")
+    allStats.sortedByDescending {
+        getField(it)
+    }.forEachIndexed { i, compareStat ->
+        val comparisonResult = if (getField(compareStat) > getField(memberStat)) {
+            "(+${getField(compareStat) - getField(memberStat)} $postfix)"
+        } else if (getField(compareStat) < getField(memberStat)) {
+            "(-${getField(memberStat) - getField(compareStat)} $postfix)"
         } else {
-            this.messages - memberStat.messages to this
+            ""
         }
+        messageBuilder.append("${i+1}. ${compareStat.user}: ${getField(compareStat)} $comparisonResult\n")
     }
-
-    val avgMessageLength = with(memberStats.maxBy { it.messageLengthAvg } ?: throw commonException) {
-        if (memberStat.messageLengthAvg > this.messageLengthAvg) {
-            (memberStat.messageLengthAvg - this.messageLengthAvg).toInt() to memberStat
-        } else {
-            (this.messageLengthAvg - memberStat.messageLengthAvg).toInt() to this
-        }
-    }
-
-    val messagesPerDay = with(memberStats.maxBy { it.messagesPerDay } ?: throw commonException) {
-        if (memberStat.messagesPerDay > this.messagesPerDay) {
-            (memberStat.messagesPerDay - this.messagesPerDay).toInt() to memberStat
-        } else {
-            (this.messagesPerDay - memberStat.messagesPerDay).toInt() to this
-        }
-    }
-
-    val messagesPerActiveDay = with(memberStats.maxBy { it.messagesPerActiveDay } ?: throw commonException) {
-        if (memberStat.messagesPerActiveDay > this.messagesPerActiveDay) {
-            (memberStat.messagesPerActiveDay - this.messagesPerActiveDay).toInt() to memberStat
-        } else {
-            (this.messagesPerActiveDay - memberStat.messagesPerActiveDay).toInt() to this
-        }
-    }
-
-    val activeDays = with(memberStats.maxBy { it.activeDays.size } ?: throw commonException) {
-        if (memberStat.activeDays.size > this.activeDays.size) {
-            memberStat.activeDays.size - this.activeDays.size to memberStat
-        } else {
-            this.activeDays.size - memberStat.activeDays.size to this
-        }
-    }
-
-    val daysInGuild = with(memberStats.maxBy { it.daysInGuild } ?: throw commonException) {
-        if (memberStat.daysInGuild > this.daysInGuild) {
-            memberStat.daysInGuild - this.daysInGuild to memberStat
-        } else {
-            this.daysInGuild - memberStat.daysInGuild to this
-        }
-    }
-
-    return MessageBuilder()
-                .append("`[Members' statistics comparison for ${memberStat.user} and ${memberStats.map { it.user }.joinToString(separator = ",")}]`\n")
-                .append("**Total messages:** ${totalMessages.second.user} (+${totalMessages.first} messages)\n")
-                .append("**Average message length:** ${avgMessageLength.second.user} (+${avgMessageLength.first} symbols)\n")
-                .append("**Messages per day:** ${messagesPerDay.second.user} (+${messagesPerDay.first} messages)\n")
-                .append("**Messages per active day:** ${messagesPerActiveDay.second.user} (+${messagesPerActiveDay.first} messages)\n")
-                .append("**Days in guild:** ${daysInGuild.second.user} (+${daysInGuild.first} days)\n")
-                .append("**Active days:** ${activeDays.second.user} (+${activeDays.first} days)\n")
 }

bf5872de3843363c890cfd84fb8d12ea5706d65d
vchernogorov <vchernogorov@griddynamics.com>
2019-01-08 20:54:04 +0400

    Fix cache key generator in CacheManager

--- src/main/kotlin/com/vchernogorov/discordbot/cache/CacheManager.kt
+++ src/main/kotlin/com/vchernogorov/discordbot/cache/CacheManager.kt
@@ -2,6 +2,7 @@ package com.vchernogorov.discordbot.cache
 
 import com.vchernogorov.discordbot.args.GuildStatsArgs
 import com.vchernogorov.discordbot.args.MemberStatsArgs
+import com.vchernogorov.discordbot.args.StringOccurrenceArgs
 import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.Member
 import redis.clients.jedis.JedisPool
@@ -13,8 +14,23 @@ import java.util.*
 class CacheManager(val jedisPool: JedisPool, val cacheExpiration: Int) {
 
     /**
-     * Creates a key from [guild] and [args]. Grabs the value by created key from [JedisPool]
-     * and converts it to [T] type using provided [mapper].
+     * Creates a key from [StringOccurrenceArgs.command], [Guild.id] and [StringOccurrenceArgs.strings].
+     * Grabs the value by created key from [JedisPool] and converts it to [T] type using provided [mapper].
+     * Returns the value associated with the key or null if no value found by the generated key.
+     */
+    fun <T> getFromCache(guild: Guild, args: StringOccurrenceArgs, mapper: (String) -> T): T? {
+        return jedisPool.resource.use {
+            val strings = args.strings.map {
+                it.joinToString(separator = "&") }.joinToString(separator = "|")
+            val key = "${args.command}/${guild.id}/$strings"
+            val value = it.get(key)
+            if (value == null) null else mapper.invoke(value)
+        }
+    }
+
+    /**
+     * Creates a key from [GuildStatsArgs.command] and [Guild.id].
+     * Grabs the value by created key from [JedisPool] and converts it to [T] type using provided [mapper].
      * Returns the value associated with the key or null if no value found by the generated key.
      */
     fun <T> getFromCache(guild: Guild, args: GuildStatsArgs, mapper: (String) -> T): T? {
@@ -26,17 +42,17 @@ class CacheManager(val jedisPool: JedisPool, val cacheExpiration: Int) {
     }
 
     /**
-     * Creates a key from [member] and [args]. Grabs the value by created key from [JedisPool]
-     * and converts it to [T] type using provided [mapper].
+     * Creates a key from [MemberStatsArgs.command], [Member.getGuild] and [Member.getUser].
+     * Grabs the value by created key from [JedisPool] and converts it to [T] type using provided [mapper].
      * Returns the value associated with the key or null if no value found by the generated key.
      */
     fun <T> getFromCache(member: Member, args: MemberStatsArgs, mapper: (String) -> T): T? {
         return jedisPool.resource.use {
             val key = if (args.compare.isEmpty()) {
-                "${args.command}/${member.user.id}"
+                "${args.command}/${member.guild.id}/${member.user.id}"
             } else {
                 val membersHash = Math.abs(Arrays.hashCode(args.compare.map { it.user.id }.toTypedArray()))
-                "${args.command}/${member.user.id}/$membersHash"
+                "${args.command}/${member.guild.id}/${member.user.id}/$membersHash"
             }
             val value = it.get(key)
             if (value == null) null else  mapper.invoke(value)
@@ -44,17 +60,18 @@ class CacheManager(val jedisPool: JedisPool, val cacheExpiration: Int) {
     }
 
     /**
-     * Creates a key from [member] and [args]. Saves converted by [mapper] value to [JedisPool] using created key.
+     * Creates a key from [MemberStatsArgs.command], [Member.getGuild] and [Member.getUser].
+     * Saves converted by [mapper] value to [JedisPool] using created key.
      * Expiration time of the cached value is set by [MyArgs.cacheExpiration] parameter.
      * Returns generated key.
      */
     fun saveToCache(member: Member, args: MemberStatsArgs, value: String): String {
         return jedisPool.resource.use {
             val key = if (args.compare.isEmpty()) {
-                "${args.command}/${member.user.id}"
+                "${args.command}/${member.guild.id}/${member.user.id}"
             } else {
                 val compareHash = Math.abs(Arrays.hashCode(args.compare.map { it.user.id }.toTypedArray()))
-                "${args.command}/${member.user.id}/$compareHash"
+                "${args.command}/${member.guild.id}/${member.user.id}/$compareHash"
             }
             it.set(key, value)
             it.expire(key, cacheExpiration)
@@ -63,7 +80,8 @@ class CacheManager(val jedisPool: JedisPool, val cacheExpiration: Int) {
     }
 
     /**
-     * Creates a key from [guild] and [args]. Saves converted by [mapper] value to [JedisPool] using created key.
+     * Creates a key from [GuildStatsArgs.command] and [Guild.id].
+     * Saves converted by [mapper] value to [JedisPool] using created key.
      * Expiration time of the cached value is set by [MyArgs.cacheExpiration] parameter.
      * Returns generated key.
      */
@@ -75,4 +93,22 @@ class CacheManager(val jedisPool: JedisPool, val cacheExpiration: Int) {
             key
         }
     }
+
+    /**
+     * Creates a key from [StringOccurrenceArgs.command], [Guild.id] and [StringOccurrenceArgs.strings].
+     * Saves converted by [mapper] value to [JedisPool] using created key.
+     * Expiration time of the cached value is set by [MyArgs.cacheExpiration] parameter.
+     * Returns generated key.
+     */
+    fun saveToCache(guild: Guild, args: StringOccurrenceArgs, value: String): String {
+        return jedisPool.resource.use {
+            val strings = args.strings.map {
+                it.joinToString(separator = "&") }.joinToString(separator = "|")
+            val key = "${args.command}/${guild.id}/$strings"
+            it.set(key, value)
+            it.expire(key, cacheExpiration)
+            key
+        }
+    }
+
 }
